8d7088cd3ad15ab4cd377be96ec9db1618c2bd2c
hitprim <dogge.gerda2002@yandex.ru>
2025-03-21 00:56:04 +0300

    New Commit

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"time"
 	"todo-api/models"
 )
 
@@ -10,6 +11,8 @@ func Connect() *gorm.DB {
 	dsn := "host=localhost user=pia password=1234 dbname=todo_db port=5432 sslmode=disable"
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	time.Sleep(10 * time.Second)
 
 	if err != nil {
 		panic("Faild to connect to Database.")
